70358431fc8f3216702f26e4a2a9c25c1875c346
Ishan Khan <tkhanishan@gmail.com>
2024-11-06 01:03:32 +0600

    Initial commit

---
+++ 43.JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,48 @@
+console.log("Hey this is tutorial of JS");
+
+var a = 5;
+// a = a + 1;
+let b = 6;
+let c = "Ishan";
+let _a = "Subham"
+// var 55a = "Rohan"  //Not Allowed
+
+// console.log(a + b + 8);
+// console.log(typeof a, typeof b, typeof c);
+
+{
+    let a = 66;
+    console.log(a)
+}
+
+console.log(a);
+
+
+let x = "Ishan Bhai";
+let y = 23;
+let z = 2.57;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+// const a1 = 6;
+// a1 = a1 + 1;  // Not Allowed because a1 is constant
+
+
+
+//Object:
+let o = {
+    name: "ishan",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
+
