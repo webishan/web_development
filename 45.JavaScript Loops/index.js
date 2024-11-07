bac22619a15d00dfd4681cfeb462fd5ac580ecbd
Ishan Khan <tkhanishan@gmail.com>
2024-11-07 23:44:07 +0600

    Initial commit

---
+++ 45.JavaScript Loops/index.js
@@ -0,0 +1,38 @@
+console.log("I am a tutorial on loops")
+
+let a = 1
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
+
+// let obj = {
+//     name: "Ishan",
+//     role: "Programmer",
+//     company: "DotQuanta"
+// }
+
+// for (const key in obj) {
+//         console.log(key);
+        
+// }
+
+// for (const c of "Ishan") {
+//     console.log(c)
+// }
+
+// let i = 0
+// while (i < 6000) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10
+do {
+    console.log(i)
+    i++;
+} while (i < 6);
