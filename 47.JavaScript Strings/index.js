914ad8d7d916911001c5cb2802a5a359564552dc
Ishan Khan <tkhanishan@gmail.com>
2024-11-17 19:34:34 +0600

    JavaScript Strings

---
+++ 47.JavaScript Strings/index.js
@@ -0,0 +1,29 @@
+console.log("This is string tutorial")
+let a = "Ishan";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+// console.log(a[5])
+console.log(a.length)
+
+//Template Literals:
+let real_name = "Ishan"
+let friend = "Shohan"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+
+let b = "ShivamSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+
+console.log(b.replace("Sh","77"))
+console.log(b.concat(a,"Jerin","Jakiya","Shithi"))
+
+console.log(b)
+
