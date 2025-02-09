94b5d309655001bbc91f231adc8abf7b37d5a3b1
Ishan Khan <tkhanishan@gmail.com>
2025-02-09 20:37:13 +0600

    CommonJs Vs EcmaScript Modules

---
+++ 68.CommonJs Vs EcmaScript Modules/mymodule.js
@@ -0,0 +1,20 @@
+export const a = 1  //named export
+export const b = 2  //named export
+export const c = 3  //named export
+export const d = 4  //named export
+export const e = 5  //named export
+
+
+// let obj;
+
+// export default obj = {
+//     x: 5,
+//     y: 7
+// }
+
+const obj = {
+    x: 5,
+    y: 7
+}
+
+export default obj;  // default export
